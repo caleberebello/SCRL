31c5a6eae1dc1650c7a2e871451e6eb1cc2b54f5
Calebe Rebello <caleberoberto2@gmail.com>
2021-12-08 18:25:54 -0400

    Atualizando

--- myapi.py
+++ myapi.py
@@ -1,9 +1,17 @@
 import flask, requests
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 import sys
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        lat1 = request.form["lat1"]
+        lng1 = request.form["lng1"]
+        return redirect(url_for('api_prova', lat1=lat1, lng1=lng1))
+    return render_template('index.html')
 
 @app.route('/<lat1>/<lng1>', methods=['GET'])
 def api_prova(lat1, lng1):
